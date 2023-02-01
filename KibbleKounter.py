1552cc56c8dfdf042e233669786c0a6a62aa49ec
Arin Budhram <ajb974@nyu.edu>
2023-02-01 13:47:20 -0500

    Added stub KibbleKounter library, see #4

---
+++ KibbleKounter.py
@@ -0,0 +1,3 @@
+def info():  
+    '''Prints a basic library description'''
+    print("Software library for the KibbleKounter project.")
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='KibbleKounter',
+      version='1.0',
+      description='Kibble Kounter Stub Library',
+      author='Arin Budhram',
+      author_email='ajb974@nyu.edu',
+      url='https://github.com/ajb974/Kibble-Kounter1',
+      py_modules=['KibbleKounter'],
+     )
