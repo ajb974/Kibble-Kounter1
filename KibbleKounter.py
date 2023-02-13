4fc3728921d9f5502d8dc300f126faff6e4019b1
mlivai <ml6751@nyu.edu>
2023-02-13 14:06:05 -0500

    Set up dummy functions for camera, see #9

--- KibbleKounter.py
+++ KibbleKounter.py
@@ -1,3 +1,9 @@
 def info():  
     '''Prints a basic library description'''
     print("Software library for the KibbleKounter project.")
+
+def setupCamera():
+    pass
+
+def takePicture():
+    print("A picture was taken.")
