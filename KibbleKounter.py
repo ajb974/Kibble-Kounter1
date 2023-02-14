00170e8a937c9891ded11aea68c79a48d2163b84
mlivai <ml6751@nyu.edu>
2023-02-14 15:09:17 -0500

    Filled in dummy functions with real working code, see #11

--- KibbleKounter.py
+++ KibbleKounter.py
@@ -1,9 +1,18 @@
+from time import sleep
+from picamera import PiCamera
+
 def info():  
     '''Prints a basic library description'''
     print("Software library for the KibbleKounter project.")
 
 def setupCamera():
-    pass
+    camera = PiCamera()
+    camera.resolution = (2592, 1944)
+    camera.framerate = 15
 
 def takePicture():
+    camera.start_preview()
+    sleep(5)
+    campera.capture('/home/pi/Desktop/piimage.jpg')
+    camera.stop_preview()
     print("A picture was taken.")
