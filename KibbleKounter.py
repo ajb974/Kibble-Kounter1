6cc4b58668393a0a3a17f45c9323c5928082722a
mlivai <ml6751@nyu.edu>
2023-02-14 15:39:07 -0500

    Second update for filling in dummy functions, see #11

--- KibbleKounter.py
+++ KibbleKounter.py
@@ -8,12 +8,20 @@ def info():
     print("Software library for the KibbleKounter project.")
 
 def setupCamera():
+    #Sets up Camera
+    #Sets resolutions of camera to highest resolution
     camera.resolution = (2592, 1944)
+    #Sets framerate to support highest resolution
     camera.framerate = 15
 
 def takePicture():
+    #takes a still (image) using camera
+    #starts live display of cameras input
     camera.start_preview()
+    #delay of 5 seconds
     sleep(5)
+    #captures the still and saves it to desktop
     campera.capture('/home/pi/Desktop/piimage.jpg')
+    #stops camera input
     camera.stop_preview()
     print("A picture was taken.")
