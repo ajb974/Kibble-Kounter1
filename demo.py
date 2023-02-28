7945aab585c96c9b49adb09949d35c7feb5dd806
Arin Budhram <ajb974@nyu.edu>
2023-02-28 02:13:14 -0500

    Added code to test force senistive resistor

--- demo.py
+++ demo.py
@@ -1,6 +1,19 @@
 import KibbleKounter
 import time
 import traceback
+import spidev
+
+def Adc_read(spi): #reads from channel 0, given spi object
+    read=spi.xfer2([0x01,0b10000000,0x0])
+    read[1]&=0b11
+    read[1]=read[1]<<8
+    final=read[2]
+    final+=read[1]
+    #print(str(final))
+    time.sleep(0.25)
+    return final
+
+
 try:
     camera = KibbleKounter.setupCamera()
     fname= KibbleKounter.takePicture(camera,'picture1')
@@ -9,12 +22,21 @@ except Exception:
     traceback.print_exc()
 
 try: 
-    input("Zero Scale") 
+    spi=spidev.SpiDev()
+    spi.open(0,0)
+    spi.max_speed_hz = 100000
+    spi.mode=0b00
+
+    input("Make sure the scale has no weight, then press enter") 
     KibbleKounter.zeroScale() 
-    input("Test Weight") 
+    input("Place 100g test weight on scale, then press enter") 
     KibbleKounter.TestScale() 
     while True:
-        print(str(KibbleKounter.readScale_grams()))
+        print("Scale reading "+str(KibbleKounter.readScale_grams())+ " grams")
+        print("Adc reading "+str(Adc_read(spi)))
         time.sleep(0.5)
 except Exception:
     traceback.print_exc()
+
+
+
