122338fa50745d583479a801a65138438e036aa3
mlivai <ml6751@nyu.edu>
2023-04-10 12:11:12 -0400

    start application layer #24

---
+++ app.py
@@ -0,0 +1,32 @@
+import KibbleKounter
+import threading import Thread, Event, Lock
+from queue import Queue
+
+#one time
+camera = KibbleKounter.setupCamera()
+KibbleKounter.setupProximitySensor()
+
+#runs continuously in the background
+
+#measure water level in a loop
+
+#measure food weight in a loop
+
+
+#runs conditionally
+
+#calibrate water bowl
+
+#calibrate food bowl
+
+#if water level changes by a certain threshold then something is done 
+
+#if food bowl amount changes...
+
+#function that takes picture and saves for setup
+
+
+
+
+
+
